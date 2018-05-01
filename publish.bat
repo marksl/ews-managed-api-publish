7fc729bda625fb26db665b9539331329f452d5fe
Mark Lamley <mark.lamley@gmail.com>
2018-04-30 20:01:26 -0700

    Scripts to push 2.2.1.1

---
+++ publish.bat
@@ -0,0 +1,24 @@
+rd /s /q ews-managed-api
+ 
+:: clone repo
+git clone https://github.com/OfficeDev/ews-managed-api.git ews-managed-api
+git checkout 812974b42be9fc046efc6434f975f71fc1b3d43c
+ 
+copy Microsoft.Exchange.WebServices.Data.nuspec ews-managed-api
+ 
+:: https://docs.microsoft.com/en-us/visualstudio/install/tools-for-managing-visual-studio-instances
+FOR /f "usebackq tokens=*" %%i IN (
+    `vswhere -version ^[15.0^,16.0^) -products * -requires Microsoft.Component.MSBuild -property installationPath`
+) DO (SET VSINSTALLDIR=%%i)
+ 
+:: VS 2017 Tools Command Prompt
+CALL "%VSINSTALLDIR%\Common7\Tools\VsDevCmd.bat"
+ 
+:: Displays 15.6.82.30579
+msbuild -version
+ 
+msbuild ews-managed-api\Microsoft.Exchange.WebServices.Data.csproj /p:Configuration=Debug
+
+powershell -Command "Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile nuget.exe"
+ 
+nuget.exe pack ews-managed-api\Microsoft.Exchange.WebServices.Data.csproj
